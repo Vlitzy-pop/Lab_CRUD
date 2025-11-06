bc69e603a8c747d7d32ff3d96eb1451339a94e54
Blitzy-pop <blitzzed2004@gmail.com>
2025-11-05 18:11:10 -0600

    Fase 2

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+class BlogListView(LoginRequiredMixin , ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    login_url = 'login'
+
+class BlogDetailView(LoginRequiredMixin , DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+    login_url = 'login'
+    
 # Create your views here.
